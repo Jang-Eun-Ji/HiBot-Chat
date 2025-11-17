6a4786321abacaeea7b5401020ffe68d993ad16e
Jang-Eun-Ji <jej132456@gmail.com>
2025-11-17 13:02:36 +0900

    [fix] cors open all including credentials

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -32,7 +32,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],      # 모든 도메인 허용
-    allow_credentials=False,  
+    allow_credentials=True,  
     allow_methods=["*"],      # 모든 HTTP 메서드 허용
     allow_headers=["*"],      # 모든 헤더 허용
 )
